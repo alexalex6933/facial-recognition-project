88c7608e3f5d002d5973feeb5890c2b4828b41a6
Alexander <z5477782@ad.unsw.edu.au>
2025-02-17 21:18:01 +1100

    Added the springboot project directory and files from Spring Initializr

---
+++ springboot/facial-recognition-backend/src/main/java/com/example/facial_recognition_backend/FacialRecognitionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.facial_recognition_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialRecognitionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacialRecognitionBackendApplication.class, args);
+	}
+
+}
